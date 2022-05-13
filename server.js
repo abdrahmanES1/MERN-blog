f66fca19299318bfcfac93c701c576d2f22bc002
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2022-05-13 20:36:15 +0100

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require("dotenv").config({ path: ".env.production" });
+require("dotenv").config({ path: ".env.local" });
 const express = require("express");
 const app = express();
+
 const morgan = require("morgan");
 const connectDB = require("./config/database");
 const cors = require("cors");
@@ -13,6 +14,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+
 connectDB();
 
 app.use(express.static("public"));
@@ -20,8 +22,18 @@ app.use("/api/posts", postRouter);
 app.use("/api/users", UserRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/uploads", uploadRouter);
-
+app.use((err, req, res, next) => {
+   console.error(err.stack)
+   res.status(500).send('Something broke!')
+})
 const PORT = process.env.PORT || 3000;
+
+
+app.use((req, res, next)=>{
+
+    res.status(404).send({error:"route not found"})
+
+})
 
 app.listen(PORT, () => {
    console.log(`listening on port ${PORT} ...`);
